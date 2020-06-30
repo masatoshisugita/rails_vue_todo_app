6951dd626967c0b298ae0d9c94389dcec95d5116
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-06-30 16:50:33 +0900

    投稿機能追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,35 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
   end
 
   def new
+    @todo = Todo.new
+  end
+
+  def create    
+    @todo = Todo.new(todo_params)
+  
+    respond_to do |format|
+      if @todo.save
+        format.html { redirect_to @todo, notice: 'todo was successfully created.' }
+        format.json { render :show, status: :created, location: @todo }
+      else
+        format.html { render :new }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title,:priority,:content)
   end
 end
